40d5b0d45e177ffdc93091e5df5f990b87e72fb6
MochRiswan <moch_riswan_29rpl@studen.smktelkom-mlg.sch.id>
2021-01-08 15:01:10 +0700

    Latihan1

---
+++ src/hellotelkom/HelloTelkom.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package hellotelkom;
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class HelloTelkom {
+    public static void main(String[] args) {
+        System.out.println("Hello");//untuk menampilkan kata hello
+        System.out.println("Selamat belajar di program java");//untuk menampilkan kalimat tersebut
+    }
+    
+}
